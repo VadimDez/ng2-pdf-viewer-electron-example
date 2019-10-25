9450e592f2eb2ce16fdde7670fb7c515c8bb33c8
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2019-10-25 23:17:19 +0200

    added pdf viewer

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { HomeRoutingModule } from './home-routing.module';
+import { HomeRoutingModule } from "./home-routing.module";
 
-import { HomeComponent } from './home.component';
-import { SharedModule } from '../shared/shared.module';
+import { HomeComponent } from "./home.component";
+import { SharedModule } from "../shared/shared.module";
+import { PdfViewerModule } from "ng2-pdf-viewer";
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule]
+  imports: [CommonModule, SharedModule, HomeRoutingModule, PdfViewerModule]
 })
 export class HomeModule {}
